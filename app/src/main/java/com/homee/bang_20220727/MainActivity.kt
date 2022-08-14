30070e735ccb61573c2dded099004ff06f609324
sjshinjee <110213136+sjshinjee@users.noreply.github.com>
2022-08-14 22:35:01 +0900

    Activity 코틀린단에서 Araylist생성후 더미데이터 입력

--- app/src/main/java/com/homee/bang_20220727/MainActivity.kt
+++ app/src/main/java/com/homee/bang_20220727/MainActivity.kt
@@ -2,10 +2,16 @@ package com.homee.bang_20220727
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.homee.bang_20220727.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
